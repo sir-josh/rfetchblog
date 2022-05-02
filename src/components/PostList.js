c1c210ca3d6076bf9374a257ddb457dfe2fac2c5
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2022-05-03 00:59:13 +0100

    code: add action creator

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,11 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { fetchPosts } from '../actions';
+import { connect } from 'react-redux';
 
 class PostList extends Component {
-  render() {
-    return (
-      <div>PostList</div>
-    )
-  }
+    componentDidMount(){
+        this.props.fetchPosts();
+    }
+
+    render() {
+        return (
+            <div>PostList</div>
+        )
+    }
 }
 
-export default PostList;
+export default connect(null, fetchPosts)(PostList);
