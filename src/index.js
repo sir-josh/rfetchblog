8ab461788ed0548d31fb95a791198e9d3b950227
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2022-05-03 00:37:16 +0100

    code: add components and reducers folder with neccessary files in them

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { legacy_createStore as createStore } from 'redux';
+
+import App from './components/App';
+import reducers from './reducers';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={createStore(reducers)}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
