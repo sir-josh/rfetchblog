6ada3ce81db66341b5a0e83d56b6a505ee50a3c4
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2022-05-03 16:17:30 +0100

    code: dispatch correct values

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -4,14 +4,20 @@ import { connect } from 'react-redux';
 
 class PostList extends Component {
     componentDidMount(){
-        console.log(this.props.fetchPosts());
+        this.props.fetchPosts();
     }
 
     render() {
+        console.log(this.props.posts);
+
         return (
             <div>PostList</div>
         )
     }
 }
 
-export default connect(null, { fetchPosts })(PostList);
+const mapStateToProps = state => {
+    return { posts: state.posts }
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostList);
