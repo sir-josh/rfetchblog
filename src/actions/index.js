b7ca5506df5b6e87615d457f4a61a9cef3d178d1
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2022-05-04 00:58:36 +0100

    code: add lodash library and use it to fetch posts and users action at one call

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,13 @@
+import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+    const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    
+    //Fetch users using posts userId
+    userIds.forEach(id => dispatch(fetchUser(id)));
+}
 
 export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get('/posts');
@@ -11,3 +20,12 @@ export const fetchUser = (id) => async dispatch => {
 
     dispatch({ type: 'FETCH_USER', payload: response.data });
 }
+
+
+// Using memoize function of lodash (lookup to the understand the func)
+    // export const fetchUser = id => async dispatch => _fetchUser(id, dispatch);
+    //     const _fetchUser = _.memoize(async(id, dispatch) => {
+    //         const response = await jsonPlaceholder.get(`/users/${id}`);
+
+    //         dispatch({ type: 'FETCH_USER', payload: response.data });
+    //     });
